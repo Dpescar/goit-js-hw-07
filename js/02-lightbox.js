a2b9413c3f80e6ff55c952bbd9ba26726486436f
Dpescar <128416673+Dpescar@users.noreply.github.com>
2023-08-21 15:36:20 +0200

    main

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,4 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 function createGalleryMarkup(galleryObj) {
@@ -22,8 +21,7 @@ function createGalleryMarkup(galleryObj) {
 const galleryRef = document.querySelector(".gallery");
 galleryRef.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems));
 
-
-const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
 });
